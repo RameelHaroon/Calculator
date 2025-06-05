dbce7708a65621b6b66f8dec5d9582b66c71db1d
Rameel Haroon <rameel.haroon@flooid.com>
2025-06-05 15:07:38 +0500

    Made the calculator functional

--- js/javascript.js
+++ js/javascript.js
@@ -1,23 +1,84 @@
 const numberKeyNode = document.querySelectorAll(".key-number");
 const operatorKeyNode = document.querySelectorAll(".key-operator");
-const displayValue = document.querySelector(".display");
+let leftOperand, rightOperand, operator, result
+
+// Display related stuff
+const display = document.querySelector(".display");
+
+function sendToDisplay(displayText){
+    display.textContent = displayText;
+}
+
+function clearDisplay() {
+    display.textContent = "";
+}
+
+function resetValues() {
+    leftOperand = undefined;
+    rightOperand = undefined;
+    operator = undefined;
+    result = undefined;
+}
+
+
+Array.from(numberKeyNode).forEach((button) => {
+    button.addEventListener("click", () => {
+        clearDisplay();
+        sendToDisplay(button.textContent);
+    });
+});
+
+Array.from(operatorKeyNode).forEach((button) => {
+    button.addEventListener("click", () => {
+        // For case <operand> <operator> eg: 1 +
+        if (
+            leftOperand === undefined 
+            && operator === undefined
+            && display.textContent !== ""
+        ) {
+            log("<operand> <operator> eg: 1 +")
+            leftOperand = parseInt(display.textContent);
+            operator = button.textContent;
+            clearDisplay();
+        }
+    });
+});
+
+// logging function
+function log(message) {
+    console.log(message);
+} 
+
+// Reset
 const clearBtn = document.querySelector(".key-clear");
-
-Array.from(numberKeyNode).forEach((node) => {
-    node.addEventListener("click", () => {
-        const nodeValue = node.getAttribute('data-choice');
-        displayValue.innerHTML += nodeValue;
-    });
+clearBtn.addEventListener("click", () =>  {
+    clearDisplay();
+    resetValues();
 });
 
-Array.from(operatorKeyNode).forEach((node) => {
-    node.addEventListener("click", () => {
-        const nodeValue = node.getAttribute('data-choice');
-        displayValue.innerHTML += " " + nodeValue + " ";
-    });
+
+const equalsToKey = document.querySelector(".key-equal");
+equalsToKey.addEventListener("click", () => {
+    if (
+        leftOperand != undefined
+        && operator != undefined
+        && display.textContent !== ""
+    ) {
+        result = getResult();
+        clearDisplay();
+        // convert number to string to check if the result overflows display
+        sendToDisplay(String(result));
+        // console logging
+        log(`${leftOperand} ${operator} ${rightOperand} = ${result}`);
+        resetValues();
+    }
 });
 
-clearBtn.addEventListener("click", () =>  displayValue.innerHTML = "");
+function getResult() {
+    rightOperand = parseInt(display.textContent);
+    return operate(operator, leftOperand, rightOperand);
+}
+
 
 function add(a, b){
     return a + b;
@@ -35,15 +96,15 @@ function divide(a, b){
     return b === 0 ? 0 : a / b;
 }
 
-function operate(operand1, operand2, operator){
+function operate(operator, leftOperand, rightOperand){
     if(operator === "+"){
-        return add(operand1, operand2);
+        return add(leftOperand, rightOperand);
     }else if(operator === "-"){
-        return subtract(operand1, operand2);
+        return subtract(leftOperand, rightOperand);
     }else if(operator === "*"){
-        return multiply(operand1, operand2);
+        return multiply(leftOperand, rightOperand);
     }else if(operator === "/"){
-        return divide(operand1, operand2);
+        return divide(leftOperand, rightOperand);
     }
 
     return 0;
